58ed2546e48be47b72934904e41289e5eb86536d
bulefire_fox <2592718594@qq.com>
2026-02-06 15:05:50 +0800

    py test mcp server

---
+++ src/test/mcp_server/main.py
@@ -0,0 +1,13 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Demo")
+
+
+@mcp.tool()
+def add(a: int, b: int):
+    """ add two numbers """
+    return a + b
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
