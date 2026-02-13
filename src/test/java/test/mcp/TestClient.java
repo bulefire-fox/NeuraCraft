94a896c453c63d87aac16611ad0b5e05738052bb
bulefire_fox <2592718594@qq.com>
2026-02-13 13:24:27 +0800

    update

--- src/test/java/test/mcp/TestClient.java
+++ src/test/java/test/mcp/TestClient.java
@@ -1,61 +1,72 @@
 package test.mcp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.ServerParameters;
+import io.modelcontextprotocol.client.transport.StdioClientTransport;
+import io.modelcontextprotocol.json.jackson.JacksonMcpJsonMapper;
+import io.modelcontextprotocol.spec.McpClientTransport;
+import io.modelcontextprotocol.spec.McpSchema;
+import lombok.extern.log4j.Log4j2;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+
+@Log4j2
 public class TestClient {
     public static void main(String[] args) throws IOException {
-        System.out.println("start p");
-        List<String> list = new ArrayList<>();
-        list.add("E:/files/project/NeuraCraft/NeuraCraft-main/src/test/mcp_server/main.py");
-        list.add("--local-timezone=Asia/Shanghai");
-        list.add(0 ,"python");
-        ProcessBuilder pb = new ProcessBuilder(list);
-        pb.redirectError(ProcessBuilder.Redirect.PIPE);
-        Process p = pb.start();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        BufferedReader errorReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-        PrintWriter writer = new PrintWriter(p.getOutputStream(), true);
-        Scanner sc = new Scanner(System.in);
+        ServerParameters params = ServerParameters.builder("python")
+                                                  .args("E:/files/project/NeuraCraft/NeuraCraft-main/src/test/mcp_server/main.py")
+                                                  .build();
+        McpClientTransport transport = new StdioClientTransport(params, new JacksonMcpJsonMapper(new ObjectMapper()));
         
-        Thread readeLine = new Thread(() -> {
-            try {
-                while (true) {
-                    String line = reader.readLine();
-                    System.out.println("readeLine: "+line);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        McpSyncClient client = McpClient.sync(transport)
+                                        .requestTimeout(Duration.ofSeconds(5))
+                                        .capabilities(
+                                                McpSchema.ClientCapabilities
+                                                        .builder()
+                                                        .roots(true)
+                                                        .build()
+                                        )
+                                        .build();
         
-        Thread readErrorLine = new Thread(() -> {
-            try {
-                while (true) {
-                    String line = errorReader.readLine();
-                    System.out.println("readErrorLine: "+line);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        client.initialize();
         
-        readeLine.start();
-        readErrorLine.start();
+        McpSchema.ListToolsResult tools = client.listTools();
         
-        while (true) {
-            System.out.println("input start");
-            String inLine = sc.nextLine();
-            if (inLine.equals("exit"))
-                break;
-            writer.println(inLine);
-            writer.flush();
-            System.out.println("input end");
+        log.info("Tools:");
+        tools.tools().forEach(tool -> log.info(tool.name()));
+        McpSchema.Tool tool = tools.tools().get(0);
+        McpSchema.JsonSchema inputSchema = tool.inputSchema();
+        log.info("Input schema:");
+        for (Map.Entry<String, Object> entry : inputSchema.properties().entrySet()) {
+            String name = entry.getKey();
+            log.info("name: {}", name);
+            Object value = entry.getValue();
+            Map<String, Object> properties = (Map<String, Object>) value;
+            log.info("properties: {}",properties);
+            log.info("properties entry : {}",properties.entrySet());
+            log.info("title: {}, type: {}", properties.get("title"), (String) properties.get("type"));
         }
+        log.info("Tools end");
+        
+        McpSchema.CallToolResult result = client.callTool(
+                McpSchema.CallToolRequest
+                        .builder()
+                        .name("add")
+                        .arguments(Map.of("a", 1, "b", 2))
+                        .build()
+        );
+        
+        result.content().forEach(content -> {
+            if (content.type().equals("text")) {
+                McpSchema.TextContent textContent = (McpSchema.TextContent) content;
+                log.info(textContent.text());
+            }
+        });
+        
+        client.close();
     }
 }
